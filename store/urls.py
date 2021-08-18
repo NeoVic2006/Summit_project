a2159ebba0ced93ced7d5f3fbfb6e924e66fea4d
kanchan-pat <82234347+kanchan-pat@users.noreply.github.com>
2021-08-18 11:20:37 -0400

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('create/', CategoryCreate.as_view(), name='category_create'),           
     re_path(r'^(?P<pk>\d+)/update/$', CategoryUpdate.as_view(), name='category_update'),
     path('products/', Products.as_view(), name='list_products'), 
-    path('<slug:slug>/', CategoryDetail.as_view(), name='category_details'),  
+    path('<slug:slug>/', CategoryDetail.as_view(), name='category_details'),   # here you are passing around the slug value of category. So you can filter() the products based on this field. 
     #re_path('<str:category_name>/',)
     
 ]
